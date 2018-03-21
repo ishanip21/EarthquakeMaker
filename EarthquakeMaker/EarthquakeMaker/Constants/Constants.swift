5dd200007b5aa19c69fed3f87569b32c8cdce15e
Ishan <ishan.g@softcodeit.com>
2018-03-22 01:42:15 +1100

    * Implemented basic UI designs

---
+++ EarthquakeMaker/EarthquakeMaker/Constants/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  EarthquakeMaker
+//
+//  Created by Ishan on 3/22/18.
+//  Copyright © 2018 Ishan. All rights reserved.
+//
+
+import UIKit
+
+class Constants: NSObject {
+    
+
+}
--- EarthquakeMaker/EarthquakeMaker/Controllers/EMHomeViewController.swift
+++ EarthquakeMaker/EarthquakeMaker/Controllers/EMHomeViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
-class EMHomeViewController: UIViewController {
+class EMHomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    enum SelectedMenuType {
+        case Map
+        case List
+    }
 
+    @IBOutlet weak var earthquakeTableView: UITableView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    var selectedMenuType = SelectedMenuType.Map
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +31,49 @@ class EMHomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - IBAction
+    
+    @IBAction func indexChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            selectedMenuType = .Map
+        } else {
+            selectedMenuType = .List
+        }
+        earthquakeTableView.reloadData()
+        print("index \(sender.selectedSegmentIndex)")
     }
-    */
-
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if selectedMenuType == .Map {
+            return 1
+        } else {
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if selectedMenuType == .Map {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "EarthQuakeCell", for: indexPath) as? UITableViewCell  else {
+                fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+            }
+            return cell;
+        } else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "MapCell", for: indexPath) as? UITableViewCell  else {
+                fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+            }
+            return cell;
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if selectedMenuType == .Map {
+            return 60
+        } else {
+            return view.frame.size.height - 60
+        }
+        
+    }
+    
 }
